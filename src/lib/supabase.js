e19b85bc8e8bd8ab16085cbe26dc78cab4824297
LUCIANO <luciano@iMac-de-LUCIANO.local>
2026-05-26 13:27:23 +0200

    configurar variables supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "https://TU-PROYECTO.supabase.co"
-
-const supabaseKey = "TU-ANON-KEY"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(
   supabaseUrl,
