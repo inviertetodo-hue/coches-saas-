37317d116a988539edcc75c15679f7daa93639a5
Luciano <inviertetodo@gmail.com>
2026-05-31 00:55:57 +0200

    phase 8.4 prepare decision pipeline flat output

--- src/services/intelligence/decisionPipeline.js
+++ src/services/intelligence/decisionPipeline.js
@@ -63,81 +63,112 @@ export function buildDecisionPipeline({
       priorityScore,
       executiveScore,
       learningBonus: learning.learningBonus,
-      historicalConfidenceScore:
-        learning.confidenceScore,
-      historicalAverageROI:
-        learning.averageROI,
-      historicalAverageProfit:
-        learning.averageProfit,
+      historicalConfidenceScore: learning.confidenceScore,
+      historicalAverageROI: learning.averageROI,
+      historicalAverageProfit: learning.averageProfit,
     },
     learning,
   });
 
-  const executiveBuySignal =
-    buildExecutiveBuySignal({
-      executiveScore,
-      successProbability:
-        successProbability.successProbability,
-      confidenceScore:
-        learning.confidenceScore,
-      liquidityScore:
-        clampScore(60 + liquidityBonus),
-      riskScore: clampScore(riskPenalty),
-    });
+  const executiveBuySignal = buildExecutiveBuySignal({
+    executiveScore,
+    successProbability: successProbability.successProbability,
+    confidenceScore: learning.confidenceScore,
+    liquidityScore: clampScore(60 + liquidityBonus),
+    riskScore: clampScore(riskPenalty),
+  });
 
   const sellSpeed = buildSellSpeed({
-    successProbability:
-      successProbability.successProbability,
+    successProbability: successProbability.successProbability,
     executiveScore,
     roi,
     profit,
-    confidenceScore:
-      learning.confidenceScore,
+    confidenceScore: learning.confidenceScore,
   });
 
-  const capitalEfficiency =
-    buildCapitalEfficiency({
-      price,
-      profit,
-      roi,
-      estimatedSellDays:
-        sellSpeed.estimatedSellDays,
-      successProbability:
-        successProbability.successProbability,
-      executiveBuySignalScore:
-        executiveBuySignal.finalScore,
-    });
+  const capitalEfficiency = buildCapitalEfficiency({
+    price,
+    profit,
+    roi,
+    estimatedSellDays: sellSpeed.estimatedSellDays,
+    successProbability: successProbability.successProbability,
+    executiveBuySignalScore: executiveBuySignal.finalScore,
+  });
 
-  const inventoryRisk =
-    buildInventoryRisk({
-      estimatedSellDays:
-        sellSpeed.estimatedSellDays,
-      successProbability:
-        successProbability.successProbability,
-      capitalEfficiencyScore:
-        capitalEfficiency.capitalEfficiencyScore,
-      historicalConfidenceScore:
-        learning.confidenceScore,
-      price,
-    });
+  const inventoryRisk = buildInventoryRisk({
+    estimatedSellDays: sellSpeed.estimatedSellDays,
+    successProbability: successProbability.successProbability,
+    capitalEfficiencyScore: capitalEfficiency.capitalEfficiencyScore,
+    historicalConfidenceScore: learning.confidenceScore,
+    price,
+  });
 
   return {
     learning,
     priorityScore,
     executiveScore,
+
     successProbability,
     executiveBuySignal,
     sellSpeed,
     capitalEfficiency,
     inventoryRisk,
+
+    flat: {
+      priorityScore,
+      executiveScore,
+
+      learningBonus: learning.learningBonus,
+      historicalConfidence: learning.confidence,
+      historicalConfidenceScore: learning.confidenceScore,
+      historicalAnalyses: learning.analyses,
+      historicalAverageROI: learning.averageROI,
+      historicalAverageProfit: learning.averageProfit,
+      learningSignals: learning.signals,
+
+      successProbability: successProbability.successProbability,
+      buySignal: successProbability.buySignal,
+      expectedROI: successProbability.expectedROI,
+      expectedProfit: successProbability.expectedProfit,
+      successRiskLabel: successProbability.riskLabel,
+      successSummary: successProbability.summary,
+      successEngine: successProbability,
+
+      executiveBuySignal,
+      executiveBuySignalScore: executiveBuySignal.finalScore,
+      executiveBuySignalLabel: executiveBuySignal.signal,
+      executiveBuySignalColor: executiveBuySignal.color,
+      executiveBuySignalSummary: executiveBuySignal.summary,
+
+      sellSpeed,
+      sellSpeedScore: sellSpeed.sellSpeedScore,
+      estimatedSellDays: sellSpeed.estimatedSellDays,
+      speedLabel: sellSpeed.speedLabel,
+      sellSpeedSummary: sellSpeed.summary,
+
+      capitalEfficiency,
+      capitalEfficiencyScore: capitalEfficiency.capitalEfficiencyScore,
+      capitalEfficiencyLabel: capitalEfficiency.capitalEfficiencyLabel,
+      capitalVelocity: capitalEfficiency.capitalVelocity,
+      annualizedProfitPotential:
+        capitalEfficiency.annualizedProfitPotential,
+      capitalRisk: capitalEfficiency.capitalRisk,
+      profitPerDay: capitalEfficiency.profitPerDay,
+      profitPerThousand: capitalEfficiency.profitPerThousand,
+      capitalEfficiencySummary: capitalEfficiency.summary,
+
+      inventoryRisk,
+      inventoryRiskScore: inventoryRisk.inventoryRiskScore,
+      inventoryRiskLabel: inventoryRisk.inventoryRiskLabel,
+      immobilizedCapitalRisk: inventoryRisk.immobilizedCapitalRisk,
+      inventoryRiskSummary: inventoryRisk.summary,
+    },
   };
 }
 
 function safeNumber(value) {
   const number = Number(value);
-  return Number.isFinite(number)
-    ? number
-    : 0;
+  return Number.isFinite(number) ? number : 0;
 }
 
 function clampScore(value) {
